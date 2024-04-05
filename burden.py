3c937a818bea426f2b7c8725c8c49678f29d04fb
Adrian Lison <adrian.lison@bsse.ethz.ch>
2024-04-05 08:29:19 +0200

    Rename life_remaining to years_remaining

--- burden.py
+++ burden.py
@@ -82,17 +82,17 @@ class Burden:
     
 class LifeExpectancy:
 
-    def __init__(self, life_remaining_males, life_remaining_females):
+    def __init__(self, years_remaining_males, years_remaining_females):
         """
         Initialize the life expectancy object
 
         Parameters
         ----------
-        life_remaining_males: numpy array with expected remaining male life years for each age, starting from age 0
-        life_remaining_females: numpy array with expected remaining male life years for each age, starting from age 0
+        years_remaining_males: numpy array with expected remaining male life years for each age, starting from age 0
+        years_remaining_females: numpy array with expected remaining male life years for each age, starting from age 0
         """
-        self.life_remaining_males = life_remaining_males
-        self.life_remaining_females = life_remaining_females
+        self.years_remaining_males = years_remaining_males
+        self.years_remaining_females = years_remaining_females
         return
     
     def get_life_expectancy(self, age, sex):
@@ -101,15 +101,15 @@ class LifeExpectancy:
         """
         
         if sex == "male":
-            if age < len(self.life_remaining_males):
-                return age + self.life_remaining_males[int(age)]
+            if age < len(self.years_remaining_males):
+                return age + self.years_remaining_males[int(age)]
             else:
-                return age + self.life_remaining_males[-1]
+                return age + self.years_remaining_males[-1]
         elif sex == "female":
-            if age < len(self.life_remaining_females):
-                return age + self.life_remaining_females[int(age)]
+            if age < len(self.years_remaining_females):
+                return age + self.years_remaining_females[int(age)]
             else:
-                return age + self.life_remaining_females[-1]
+                return age + self.years_remaining_females[-1]
         else:
             raise ValueError("Sex is binary here, must be either 'male' or 'female'.")
         
@@ -119,14 +119,14 @@ class LifeExpectancy:
         """
         
         if sex == 0: # male
-            if age < len(self.life_remaining_males):
-                return self.life_remaining_males[int(age)]
+            if age < len(self.years_remaining_males):
+                return self.years_remaining_males[int(age)]
             else:
-                return self.life_remaining_males[-1]
+                return self.years_remaining_males[-1]
         elif sex == 1: # female
-            if age < len(self.life_remaining_females):
-                return self.life_remaining_females[int(age)]
+            if age < len(self.years_remaining_females):
+                return self.years_remaining_females[int(age)]
             else:
-                return self.life_remaining_females[-1]
+                return self.years_remaining_females[-1]
         else:
             raise ValueError("Sex is binary here, must be either 0 (for male) or 1 (for female).")
